d2350bdcb28039220285a430690d8640eb5a9edd
careerroadies <deepakb.net@gmail.com>
2016-11-28 18:13:29 +0530

    Add Two models Classes for profile controller

---
+++ DataModels/profile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class profile
+    {
+
+    }
+
+    public class BasicProfileDetails
+    {
+        string ProfileId { get; set; }
+        string UserId { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Dob { get; set; }
+        string MobileNumber { get; set; }
+        string PrimaryEmail { get; set; }
+        string AlternateEmail { get; set; }
+        string Gender { get; set; }
+        string MaritalStatus { get; set; }
+        string ProfilePicture { get; set; }
+        string ProfileText { get; set; }    
+    }
+
+    public class profileAddressDetails
+    {
+        string ProfileId { get; set; }
+        string Id { get; set; }
+        string AddressLine1 { get; set; }
+        string AddressLine2 { get; set; }
+        string Landmark { get; set; }
+        string Locality { get; set; }
+        string Pincode { get; set; }
+        int CityID { get; set; }
+        int StateID { get; set; }
+        int AddressCategoryID { get; set; }
+    }
+}
