2408b9f2a60411285cea28aaa6ea08f05eb618e2
dixitpankaj02 <dixit.pankaj02@hotmail.com>
2017-02-08 09:16:37 +0530

    Modified the Bussiness  rules

--- ApplicationService/ProfileBusinessRules.cs
+++ ApplicationService/ProfileBusinessRules.cs
@@ -8,7 +8,7 @@ using DataserviceInterface;
 
 namespace ApplicationService
 {
-    public class ProfileBusinessRules:ValidationRules
+    public class ProfileBusinessRules : ValidationRules
     {
         IProfileService profiledataservice;
 
@@ -35,53 +35,44 @@ namespace ApplicationService
         {
             profiledataservice = profileservice;
             InitializeValidationRules(profile);
-            ValidateRequired("FirstName", "First Name");
-            ValidateRequired("LastName", "Last Name");
-            ValidateRequired("Gender", "Gender");
-            ValidateRequired("state", "State");
-            ValidateRequired("city", "City");
-            ValidateRequired("location", "Location");
-            ValidateEmailAddress("PrimaryEmail", "Primary Email");
-            ValidateEmailAddress("AlternateEmail", "Alternate Email");
+            ValidateRequired("postgraduationid", "Post Graduation");
+            ValidateRequired("postgraduationtypeid", "Post Graduation Type");
+            ValidateRequired("specializationid", "Specialization");
+            ValidateRequired("UniversityorInstituteid", "University or Institute");
+            ValidateRequired("Year", "Year");
+            ValidateRequired("gradingsystem", "Grade");
+            ValidateRequired("marks", "Marks");
+
         }
         public void ValidateGraduationDetails(profilegraduationdetails profile, IProfileService profileservice)
         {
             profiledataservice = profileservice;
             InitializeValidationRules(profile);
-            ValidateRequired("FirstName", "First Name");
-            ValidateRequired("LastName", "Last Name");
-            ValidateRequired("Gender", "Gender");
-            ValidateRequired("state", "State");
-            ValidateRequired("city", "City");
-            ValidateRequired("location", "Location");
-            ValidateEmailAddress("PrimaryEmail", "Primary Email");
-            ValidateEmailAddress("AlternateEmail", "Alternate Email");
+            ValidateRequired("graduationid", "Graduation");
+            ValidateRequired("postgraduationtypeid", "Graduation Type");
+            ValidateRequired("specializationid", "Specialization");
+            ValidateRequired("UniversityorInstituteid", "University or Institute");
+            ValidateRequired("Year", "Year");
+            ValidateRequired("gradingsystem", "Grade");
+            ValidateRequired("marks", "Marks");
         }
         public void ValidateHigherSecondaryDetails(profilehighersecondarydetails profile, IProfileService profileservice)
         {
             profiledataservice = profileservice;
             InitializeValidationRules(profile);
-            ValidateRequired("FirstName", "First Name");
-            ValidateRequired("LastName", "Last Name");
-            ValidateRequired("Gender", "Gender");
-            ValidateRequired("state", "State");
-            ValidateRequired("city", "City");
-            ValidateRequired("location", "Location");
-            ValidateEmailAddress("PrimaryEmail", "Primary Email");
-            ValidateEmailAddress("AlternateEmail", "Alternate Email");
+            ValidateRequired("educationboardid", "Education Board Name");
+            ValidateRequired("medium", "Medium ");
+            ValidateRequired("Year", "Year");
+            ValidateRequired("marks", "Marks");
         }
         public void ValidateSecondaryDetails(profilesecondarydetails profile, IProfileService profileservice)
         {
             profiledataservice = profileservice;
             InitializeValidationRules(profile);
-            ValidateRequired("FirstName", "First Name");
-            ValidateRequired("LastName", "Last Name");
-            ValidateRequired("Gender", "Gender");
-            ValidateRequired("state", "State");
-            ValidateRequired("city", "City");
-            ValidateRequired("location", "Location");
-            ValidateEmailAddress("PrimaryEmail", "Primary Email");
-            ValidateEmailAddress("AlternateEmail", "Alternate Email");
+            ValidateRequired("educationboardid", "Education Board Name");
+            ValidateRequired("medium", "Medium ");
+            ValidateRequired("Year", "Year");
+            ValidateRequired("marks", "Marks");
         }
     }
 }
